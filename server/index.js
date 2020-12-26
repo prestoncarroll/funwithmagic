c1ff1e0c6e4a7421bbb43b02283189a7c158361e
prestoncarroll <plutusinc1@yahoo.com>
2020-12-26 12:06:06 -0600

    set up db listeners

--- server/index.js
+++ server/index.js
@@ -12,3 +12,16 @@ app.use(cors());
 
 const Connection_URL = Connection_URL;
 const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(Connection_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+
+  .then(() =>
+    app.listen(PORT, () => console.log(`server running on port : ${PORT}`))
+  )
+  .catch((error) => console.log(err));
+
+mongoose.set("useFindandModify", false);
