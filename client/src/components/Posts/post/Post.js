eb97fb955f19489427f7fa4b8764b1ad85649730
prestoncarroll <plutusinc1@yahoo.com>
2021-01-10 17:31:25 -0600

    adding styles

--- client/src/components/Posts/post/Post.js
+++ client/src/components/Posts/post/Post.js
@@ -1,9 +1,48 @@
 import React from "react";
+
+import {
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Button,
+  Typography,
+} from "@material-ui/core";
+import ThumbUpAltIcon from "@material-ui/icons/ThumbUpAlt";
+import DeleteIcon from "@material-ui/icons/Delete";
+import MoreHorizIcon from "@metrial-ui/icons/MoreHoriz";
+import moment from "moment";
 import useStyles from "./styles";
 
-const Post = () => {
+const Post = ({ post }) => {
   const classes = useStyles();
-  return <h1>post</h1>;
+  return (
+    <Card className={classes.card}>
+      <CardMedia
+        className={classes.media}
+        image={post.selectedFile}
+        title={post.title}
+      />
+      <div className={classes.overlay}>
+        <Typography variant="h6">{post.creator}</Typography>
+        <Typography variant="body2">
+          {" "}
+          {moment(post.createdAt).fromNow()}
+        </Typography>
+      </div>
+      <div className={classes.overlay2}>
+        <Button style={{ color: "white" }} size="small" onClick={() => {}}>
+          <MoreHorizIcon fontSize="default" />
+        </Button>
+      </div>
+
+      <div className={classes.details}>
+        <Typography variant="body2" color="textSecondary">
+          {post.tags.map((tag) => `#${tag}`)}
+        </Typography>
+      </div>
+    </Card>
+  );
 };
 
 export default Post;
