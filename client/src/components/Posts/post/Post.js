3cf06aac1ee191e4539bb3020034465cf30c5ea5
prestoncarroll <plutusinc1@yahoo.com>
2021-01-24 09:26:31 -0600

    added delete and like buttons to post

--- client/src/components/Posts/post/Post.js
+++ client/src/components/Posts/post/Post.js
@@ -10,7 +10,7 @@ import {
 } from "@material-ui/core";
 import ThumbUpAltIcon from "@material-ui/icons/ThumbUpAlt";
 import DeleteIcon from "@material-ui/icons/Delete";
-import MoreHorizIcon from "@metrial-ui/icons/MoreHoriz";
+import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
 import moment from "moment";
 import useStyles from "./styles";
 
@@ -41,6 +41,21 @@ const Post = ({ post }) => {
           {post.tags.map((tag) => `#${tag}`)}
         </Typography>
       </div>
+      <CardContent>
+        <Typography className={classes.title} variant="h5" gutterBottom>
+          {post.message}{" "}
+        </Typography>
+      </CardContent>
+      <CardActions className={classes.cardActions}>
+        <Button size="small" color="primary" onClick={() => {}}>
+          {" "}
+          <ThumbUpAltIcon fontSize="small" /> Like {post.likeCount}
+        </Button>
+        <Button size="small" color="primary" onClick={() => {}}>
+          {" "}
+          <DeleteIcon fontSize="small" /> delete{" "}
+        </Button>
+      </CardActions>
     </Card>
   );
 };
